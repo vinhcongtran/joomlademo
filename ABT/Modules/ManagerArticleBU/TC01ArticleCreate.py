78a3cdecc9b4046562e092564fc655a8939670b7
vinhcongtran <vinh.cong.tran@logigear.com>
2015-12-07 10:49:34 +0700

    Unittest

--- ABT/Modules/ManagerArticleBU/TC01ArticleCreate.py
+++ ABT/Modules/ManagerArticleBU/TC01ArticleCreate.py
@@ -4,70 +4,88 @@ Created on Dec 1, 2015
 @author: van.ngo
 '''
 
-
-
 from time import strftime, localtime
-
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.select import Select
-from selenium.webdriver.support.wait import WebDriverWait
-
-
-from ABT.Actions.Pages.NewArticle import NewArticle
-from ABT.Actions.Pages.ControlPanel import ControlPanel
-from ABT.Actions.Pages.Login import Login
-from ABT.Actions.Pages.ManagerArticle import ManagerArticle
-from ABT.Actions.CommonActions import CommonActions
+from ABT.Modules.ImportPages import ImportPages
+import unittest
+import HTMLTestRunner
 
 
-class TC01ArticleCreate(Login, ControlPanel, CommonActions, NewArticle, ManagerArticle):
+class TC01ArticleCreate(unittest.TestCase, ImportPages):
     '''
     classdocs
     '''
 
-
-    def __init__(self):
+    def setUp(self):
         '''
         Constructor
         '''
-        Login.__init__(self)
-        ControlPanel.__init__(self)
-        CommonActions.__init__(self)
-        NewArticle.__init__(self)
-        ManagerArticle.__init__(self)
+        ImportPages.__init__(self)
         self.title = "Article " + strftime("%Y-%m-%d %H:%M:%S", localtime())
         self.text = "This is an article"
         self.category = "Extensions"
         self.option = "Save & Close"
+        self.browser = None
+        
+    def tearDown(self):
+        #End conditions
+        self.logOut(self.browser)
+        self.browser.quit()
     
-    
-    def TC01(self):
+    def test_TC01(self):
         # Main steps
         # Open browser
-        browser = self.openBrowser()
+        self.browser = self.openBrowser()
           
         self.logInfo("Step 1 - Navigate to Joomla administrator admin page")
-        self.navigate(browser, self.joomlaUrl)
+        self.navigate(self.browser, self.joomlaUrl)
           
         self.logInfo("Step 2,3,4 - Log In an existing account")
-        self.login(browser, self.username, self.password)
+        self.login(self.browser, self.username, self.password)
           
         self.logInfo("Step 5 - Select Content > Article Manager")
-        self.navigateMenu(browser, "Content>Article Manager")
+        self.navigateMenu(self.browser, "Content>Article Manager")
   
         self.logInfo("Step 6,7,8,9,10: Create a new article ")
-        self.createNewArticle(browser, self.title, self.category, self.text, self.option)
+        self.createNewArticle(self.browser, self.title, self.category, self.text, self.option)
           
         #VP: 1. "Article successfully saved" message is displayed
         #VP: 2. Created article is displayed on the articles table
         self.logInfo("Step 11 -  Verify the article is saved successfully ")  
-        self.checkArticleCreated(browser, self.title)
+        self.checkArticleExist(self.browser, self.title)
+    
+    def test_TC02(self):
+        # Main steps
+        # Open browser
+        self.browser = self.openBrowser()
           
-        #End conditions
-        self.logOut(browser)
-        browser.quit()
-
-         
+        self.logInfo("Step 1 - Navigate to Joomla administrator admin page")
+        self.navigate(self.browser, self.joomlaUrl)
+          
+        self.logInfo("Step 2,3,4 - Log In an existing account")
+        self.login(self.browser, self.username, self.password)
+          
+        self.logInfo("Step 5 - Select Content > Article Manager")
+        self.navigateMenu(self.browser, "Content>Article Manager")
+  
+        self.logInfo("Step 6,7,8,9,10: Create a new article ")
+        self.createNewArticle(self.browser, self.title, self.category, self.text, self.option)
+          
+        #VP: 1. "Article successfully saved" message is displayed
+        #VP: 2. Created article is displayed on the articles table
+        self.logInfo("Step 11 -  Verify the article is saved successfully ")  
+        self.checkArticleExist(self.browser, self.title)
         
 if __name__ == '__main__':
-    TC01ArticleCreate().TC01()
+    suite = unittest.TestSuite()
+    suite.addTest(TC01ArticleCreate('test_TC01'))
+    suite.addTest(TC01ArticleCreate('test_TC02'))
+#     runner = unittest.TextTestRunner()
+#     runner.run(suite)
+#     unittest.main(suite)
+    buf = file("D:\\testcase01.html",'wb')
+    runner = HTMLTestRunner.HTMLTestRunner(
+                    stream=buf,
+                    title='TC01 - Test Results',
+                    description='TC01 result'
+                    )
+    runner.run(suite)
